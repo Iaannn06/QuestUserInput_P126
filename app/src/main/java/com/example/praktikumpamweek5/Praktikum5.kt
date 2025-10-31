8312ca5f698e88a93df7c39c004fcc26b0c30180
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2025-10-31 18:09:56 +0700

    menambahkan bentuk card

--- app/src/main/java/com/example/praktikumpamweek5/Praktikum5.kt
+++ app/src/main/java/com/example/praktikumpamweek5/Praktikum5.kt
@@ -7,7 +7,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -70,6 +72,10 @@ fun RegistrationScreen() {
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(horizontal = 8.dp),
+                shape = RoundedCornerShape(16.dp),
+                colors = CardDefaults.cardColors(containerColor = Color.White),
+                elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+
             ){
 
             }
