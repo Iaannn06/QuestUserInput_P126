9629555deeee7d8d0ebce37767be139e21affde3
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2025-10-31 18:15:41 +0700

    menambahkan spacer untuk label nama

--- app/src/main/java/com/example/praktikumpamweek5/Praktikum5.kt
+++ app/src/main/java/com/example/praktikumpamweek5/Praktikum5.kt
@@ -4,8 +4,10 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
@@ -92,6 +94,8 @@ fun RegistrationScreen() {
                         singleLine = true,
                         shape = RoundedCornerShape(8.dp)
                     )
+                    Spacer(modifier = Modifier.height(16.dp))
+
                 }
             }
         }
