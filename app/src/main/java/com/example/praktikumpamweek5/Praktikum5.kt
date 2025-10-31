69865b461fc8df9e6fc6ad392883c1f94c9909dc
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2025-10-31 17:45:04 +0700

    menambahkan variabel untuk ditampilkan

--- app/src/main/java/com/example/praktikumpamweek5/Praktikum5.kt
+++ app/src/main/java/com/example/praktikumpamweek5/Praktikum5.kt
@@ -1,8 +1,10 @@
 package com.example.praktikumpamweek5
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 @Composable
 fun RegistrationScreen() {
@@ -10,5 +12,11 @@ fun RegistrationScreen() {
     var textAlamat by remember { mutableStateOf("") }
     var selectedGender by remember { mutableStateOf("") }
     var selectedStatus by remember { mutableStateOf("") }
+
+    var submittedNama by remember { mutableStateOf("") }
+    var submittedAlamat by remember { mutableStateOf("") }
+    var submittedGender by remember { mutableStateOf("") }
+    var submittedStatus by remember { mutableStateOf("") }
+
 
 }
